5dfc4b26c7190c220e1e3715e9d7af99c50d0538
Maria Eduarda Pereira de Jesus <mariaeduardapereiraje@gmail.com>
2025-06-20 22:02:52 -0300

    feat: Integrate digital storage converter with main file

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from length import run_length_converter
 from time_duration import run_time_duration_converter
 from volume import run_volume_converter
 from number_bases import run_number_bases_converter
+from digital_storage import run_digital_storage_converter
 
 def main():
     while True:
         print()
-        print("UNIT CONVERTER\n1 - Temperature Converter\n2 - Currency Converter\n3 - Length Converter\n4 - Time and Duration Converter\n5 - Volume Converter\n6 - Number Bases Converter\n7 - Exit")
+        print("UNIT CONVERTER\n1 - Temperature Converter\n2 - Currency Converter\n3 - Length Converter\n4 - Time and Duration Converter\n5 - Volume Converter\n6 - Number Bases Converter\n7 - Digital Storage Converter\n8 - Exit")
         try:
             option = int(input("Choose an option - "))
         except ValueError:
@@ -28,6 +29,8 @@ def main():
         elif option == 6:
             run_number_bases_converter()
         elif option == 7:
+            run_digital_storage_converter()
+        elif option == 8:
             break
         else:
             print("Invalid option.")
