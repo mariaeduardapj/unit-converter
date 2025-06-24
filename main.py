940d2b289298bc9be1c870e1ca2b7da5fa137282
Maria Eduarda Pereira de Jesus <mariaeduardapereiraje@gmail.com>
2025-06-24 20:56:16 -0300

    feat: Integrate pressure converter with main file

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ from digital_storage import run_digital_storage_converter
 from weight import run_weight_converter
 from area import run_area_converter
 from energy import run_energy_converter
+from pressure import run_pressure_converter
 
 def main():
     while True:
         print()
-        print("UNIT CONVERTER\n01 - Temperature Converter       | 02 - Currency Converter | 03 - Length Converter\n04 - Time and Duration Converter | 05 - Volume Converter   | 06 - Number Bases Converter\n07 - Digital Storage Converter   | 08 - Weight Converter   | 09 - Area Converter\n10 - Energy Converter            | 11 - Exit")
+        print("UNIT CONVERTER\n01 - Temperature Converter       | 02 - Currency Converter | 03 - Length Converter\n04 - Time and Duration Converter | 05 - Volume Converter   | 06 - Number Bases Converter\n07 - Digital Storage Converter   | 08 - Weight Converter   | 09 - Area Converter\n10 - Energy Converter            | 11 - Pressure Converter | 12 - Exit")
         try:
             option = int(input("Choose an option - "))
         except ValueError:
-            print("Invalid option. Please, enter a number from 1 to 7.")
+            print("Invalid option. Please, enter a number from 1 to 12.")
             return
         print()
         if option == 1:
@@ -40,6 +41,8 @@ def main():
         elif option == 10:
             run_energy_converter()
         elif option == 11:
+            run_pressure_converter()
+        elif option == 12:
             break
         else:
             print("Invalid option.")
--- pressure.py
+++ pressure.py
@@ -1,43 +1,44 @@
-pressures = ["Pa","kPa","MPa","bar","atm","mmHg","psi"]
-factors_to_pascal = {
-    0: 1,       # Pa to Pa
-    1: 10**3,   # kPa to Pa
-    2: 10**6,   # MPa to Pa
-    3: 10**5 ,  # bar to Pa
-    4: 101.325, # atm to Pa
-    5: 133.322, # mmHg to Pa
-    6: 6894.76  # psi to Pa
-}
-factors_from_pascal = {
-    0: 1,           # Pa to Pa
-    1: 1 / 10**3,   # kPa to Pa
-    2: 1 / 10**6,   # MPa to Pa
-    3: 1 / 10**5 ,  # bar to Pa
-    4: 1 / 101.325, # atm to Pa
-    5: 1 / 133.322, # mmHg to Pa
-    6: 1 / 6894.76  # psi to Pa
-}
-print("-=-=-=-=-=- PRESSURE CONVERTER -=-=-=-=-=-")
-print("MENU OPTIONS:\n1 - Pascal (Pa)      | 2 - Kilopascal (kPa)    | 3 - Megapascal (MPa)     | 4 - Bar (bar)\n5 - Atmosphere (atm) | 6 - Millimeter of Mercury (mmHg) | 7 - Pound per Square Inch (psi)")
-try:
-    f = int(input("Convert from - "))
-    if 1 > f > 7:
-        print("Invalid option. Please choose a number between 1 and 7.")
-except ValueError:
-    print("Invalid option. Please, enter a number from 1 to 7.")
-    exit()
-try:
-    t = int(input("To - "))
-    if 1 > t > 7:
-        print("Invalid option. Please choose a number between 1 and 7.")
-except ValueError:
-    print("Invalid option. Please, enter a number from 1 to 7.")
-    exit()
-try:
-    value = float(input(f"Value in {pressures[f-1]} - "))
-except ValueError:
-    print("Invalid value. Please enter a number.")
-    exit()
-value_in_pascal = value * factors_to_pascal[f-1]
-converted_value = value_in_pascal * factors_from_pascal[t-1]
-print(f"{value} {pressures[f-1]} -> {converted_value} {pressures[t-1]}")
+def run_pressure_converter():
+    pressures = ["Pa","kPa","MPa","bar","atm","mmHg","psi"]
+    factors_to_pascal = {
+        0: 1,       # Pa to Pa
+        1: 10**3,   # kPa to Pa
+        2: 10**6,   # MPa to Pa
+        3: 10**5 ,  # bar to Pa
+        4: 101.325, # atm to Pa
+        5: 133.322, # mmHg to Pa
+        6: 6894.76  # psi to Pa
+    }
+    factors_from_pascal = {
+        0: 1,           # Pa to Pa
+        1: 1 / 10**3,   # kPa to Pa
+        2: 1 / 10**6,   # MPa to Pa
+        3: 1 / 10**5 ,  # bar to Pa
+        4: 1 / 101.325, # atm to Pa
+        5: 1 / 133.322, # mmHg to Pa
+        6: 1 / 6894.76  # psi to Pa
+    }
+    print("-=-=-=-=-=- PRESSURE CONVERTER -=-=-=-=-=-")
+    print("MENU OPTIONS:\n1 - Pascal (Pa)      | 2 - Kilopascal (kPa)    | 3 - Megapascal (MPa)     | 4 - Bar (bar)\n5 - Atmosphere (atm) | 6 - Millimeter of Mercury (mmHg) | 7 - Pound per Square Inch (psi)")
+    try:
+        f = int(input("Convert from - "))
+        if 1 > f > 7:
+            print("Invalid option. Please choose a number between 1 and 7.")
+    except ValueError:
+        print("Invalid option. Please, enter a number from 1 to 7.")
+        exit()
+    try:
+        t = int(input("To - "))
+        if 1 > t > 7:
+            print("Invalid option. Please choose a number between 1 and 7.")
+    except ValueError:
+        print("Invalid option. Please, enter a number from 1 to 7.")
+        exit()
+    try:
+        value = float(input(f"Value in {pressures[f-1]} - "))
+    except ValueError:
+        print("Invalid value. Please enter a number.")
+        exit()
+    value_in_pascal = value * factors_to_pascal[f-1]
+    converted_value = value_in_pascal * factors_from_pascal[t-1]
+    print(f"{value} {pressures[f-1]} -> {converted_value} {pressures[t-1]}")
