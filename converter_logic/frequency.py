6b6dc1cd595e6e2064d4c5ec24e4b8d599384913
Maria Eduarda Pereira de Jesus <mariaeduardapereiraje@gmail.com>
2025-06-27 18:12:18 -0300

    bug: Fixes error in frequency file conversion factors

--- converter_logic/frequency.py
+++ converter_logic/frequency.py
@@ -1,25 +1,25 @@
 frequencies = ["Hz","kHz","MHz","GHz","THz","cps","rpm","bpm","fps"]
 factors_to_hz = {
-    0: 1,      # Hz to Hz
-    1: 10**3,  # kHz to Hz
-    2: 10**6,  # MHz to Hz
-    3: 10**9,  # GHz to Hz
-    4: 10**12, # THz to Hz
-    5: 1,      # cps to Hz
-    6: 1 / 60, # rpm to Hz
-    7: 1 / 60, # bpm to Hz
-    8: 1       # fps to Hz
+    "Hz": 1,
+    "kHz": 10**3,
+    "MHz": 10**6,
+    "GHz": 10**9,
+    "THz": 10**12,
+    "cps": 1,
+    "rpm": 1 / 60,
+    "bpm": 1 / 60,
+    "fps": 1
 }
 factors_from_hz = {
-    0: 1,          # Hz to Hz
-    1: 1 / 10**3,  # Hz to kHz
-    2: 1 / 10**6,  # Hz to MHz
-    3: 1 / 10**9,  # Hz to GHz
-    4: 1 / 10**12, # Hz to THz
-    5: 1,          # Hz to cps
-    6: 60,         # Hz to rpm
-    7: 60,         # Hz to bpm
-    8: 1           # Hz to fps
+    "Hz": 1,
+    "kHz": 1 / 10**3,
+    "MHz": 1 / 10**6,
+    "GHz": 1 / 10**9,
+    "THz": 1 / 10**12,
+    "cps": 1,
+    "rpm": 60,
+    "bpm": 60,
+    "fps": 1
 }
 def convert_frequency(value,from_unit,to_unit):
     if from_unit not in factors_from_hz or to_unit not in factors_from_hz:
