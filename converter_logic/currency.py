55f6668864db22cce0a4cef1077510fd7e670d61
Maria Eduarda Pereira de Jesus <mariaeduardapereiraje@gmail.com>
2025-06-28 16:52:29 -0300

    style: corrects standardization

--- converter_logic/currency.py
+++ converter_logic/currency.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 currencies = ["USD", "EUR", "GBP", "JPY", "BRL", "CNY"]
 
-def currency_converter(f, to, value):
+def convert_currency(f, to, value):
     load_dotenv()
     access_key = os.getenv("API_KEY")
     url = f"https://api.apilayer.com/exchangerates_data/convert?from={f}&to={to}&amount={value}"
